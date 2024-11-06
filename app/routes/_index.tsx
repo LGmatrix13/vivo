15e6afdcc00c3ad85bc2946100dbc221fb12ad91
hensleyas21 <hensleyas21@gcc.edu>
2024-11-06 14:50:26 -0500

    Added images

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import logo from "../images/logo-white.png"
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,7 +12,12 @@ export default function Index() {
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="flex flex-col text-center space-y-3">
-        <h1 className="font-bold text-2xl">Vivo</h1>
+        <div className="justify-center flex">
+          <a href="https://www.gcc.edu/Home/Staff-Directory/Staff-Detail/christopher-merrick" target="_blank">
+            <img className="object-contain h-40" src={logo} />
+          </a>
+        </div>
+        
         <p>Live, Laugh, Love</p>
       </div>
     </div>
