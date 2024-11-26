f574cf8e8cb5e686e072ae7c48820209b669ae53
hensleyas21 <hensleyas21@gcc.edu>
2024-11-26 13:15:47 -0500

    RAs and RDs page stubs for admin people

---
+++ app/routes/admin.people.ras.tsx
@@ -0,0 +1,5 @@
+import Table from "~/components/common/Table";
+
+export default function AdminPeopleRDsPage() {
+  return <Table columnKeys={{}} rows={[]} rowKeys={{}} />;
+}
---
+++ app/routes/admin.people.rds.tsx
@@ -0,0 +1,5 @@
+import Table from "~/components/common/Table";
+
+export default function AdminPeopleRAsPage() {
+  return <Table columnKeys={{}} rows={[]} rowKeys={{}} />;
+}
