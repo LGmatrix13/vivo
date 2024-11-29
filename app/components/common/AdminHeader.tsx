6bfa12e7cd28cb41ca3fa84ae3dd66fa32b82986
Liam Grossman <liam.j.grossman@gmail.com>
2024-11-29 11:12:26 -0500

    Reduce code in AdminHeader

--- app/components/common/AdminHeader.tsx
+++ app/components/common/AdminHeader.tsx
@@ -2,33 +2,47 @@ import { useLocation } from "react-router-dom";
 import { Link } from "@remix-run/react";
 import { Chart, File, Home, Logo, Calendar, Users, Door } from "./Icons";
 
-const paths = {
-  schedules: {
-    default: "/admin/schedules/schedule",
-    parent: "/admin/schedules",
-  },
-  reports: {
-    default: "/admin/reports/conversation",
-    parent: "/admin/reports",
-  },
-  insights: "/admin/insights",
-  rcis: {
-    default: "/admin/rcis/complete",
-    parent: "/admin/rcis",
-  },
-  people: {
-    default: "/admin/people/residents",
-    parent: "/admin/people",
-  },
-  housing: {
-    default: "/admin/housing/buildings",
-    parent: "/admin/housing",
-  },
-};
-
 export default function AdminHeader() {
   const location = useLocation();
   const currentPath = location.pathname;
+  const routes = [
+    {
+      name: "Schedules",
+      Icon: Calendar,
+      default: "/admin/schedules/schedule",
+      parent: "/admin/schedules",
+    },
+    {
+      name: "Reports",
+      Icon: File,
+      default: "/admin/reports/conversation",
+      parent: "/admin/reports",
+    },
+    {
+      name: "Insights",
+      Icon: Chart,
+      default: "/admin/insights",
+      parent: "/admin/insights",
+    },
+    {
+      name: "RCIs",
+      Icon: Door,
+      default: "/admin/rcis/complete",
+      parent: "/admin/rcis",
+    },
+    {
+      name: "People",
+      Icon: Users,
+      default: "/admin/people/residents",
+      parent: "/admin/people",
+    },
+    {
+      name: "Housing",
+      Icon: Home,
+      default: "/admin/housing/buildings",
+      parent: "/admin/housing",
+    },
+  ];
 
   function isActivePage(path: string) {
     return currentPath.startsWith(path) ? "border-b-2 border-blue-600" : "";
@@ -37,70 +51,22 @@ export default function AdminHeader() {
   return (
     <header className="border-b mb-7">
       <nav className="max-w-screen-2xl mx-auto flex items-center px-10 justify-between">
-        <Link to={paths.schedules.default}>
+        <Link to="/schedules/schedule">
           <Logo />
         </Link>
         <div className="ml-auto order-2 flex space-x-5 text-lg">
-          <Link to={paths.schedules.default}>
-            <button
-              className={`${isActivePage(
-                paths.schedules.parent
-              )} py-7 flex items-center space-x-2`}
-            >
-              <Calendar />
-              <p className="font-bold">Schedules</p>
-            </button>
-          </Link>
-          <Link to={paths.reports.default}>
-            <button
-              className={`${isActivePage(
-                paths.reports.parent
-              )} py-7 flex items-center space-x-2`}
-            >
-              <File />
-              <p className="font-bold">Reports</p>
-            </button>
-          </Link>
-          <Link to={paths.insights}>
-            <button
-              className={`${isActivePage(
-                paths.insights
-              )} py-7 flex items-center space-x-2`}
-            >
-              <Chart />
-              <p className="font-bold">Insights</p>
-            </button>
-          </Link>
-          <Link to={paths.rcis.default}>
-            <button
-              className={`${isActivePage(
-                paths.rcis.parent
-              )} py-7 flex items-center space-x-2`}
-            >
-              <Door />
-              <p className="font-bold">RCIs</p>
-            </button>
-          </Link>
-          <Link to={paths.people.default}>
-            <button
-              className={`${isActivePage(
-                paths.people.parent
-              )} py-7 flex items-center space-x-2`}
-            >
-              <Users />
-              <p className="font-bold">People</p>
-            </button>
-          </Link>
-          <Link to={paths.housing.default}>
-            <button
-              className={`${isActivePage(
-                paths.housing.parent
-              )} py-7 flex items-center space-x-2`}
-            >
-              <Home />
-              <p className="font-bold">Housing</p>
-            </button>
-          </Link>
+          {routes.map((route) => (
+            <Link to={route.default}>
+              <button
+                className={`${isActivePage(
+                  route.parent
+                )} py-7 flex items-center space-x-2`}
+              >
+                <route.Icon />
+                <p className="font-bold">{route.name}</p>
+              </button>
+            </Link>
+          ))}
         </div>
       </nav>
     </header>
