56fa0a7f20ab276a830b8906c90e18e3303732a7
hensleyas21 <hensleyas21@gcc.edu>
2024-12-04 14:39:34 -0500

    Added ARD people view

---
+++ app/routes/admin.people.ards.tsx
@@ -0,0 +1,109 @@
+import { ActionFunctionArgs } from "@remix-run/node";
+import { Await, defer, redirect, useLoaderData } from "@remix-run/react";
+import { sql, aliasedTable, eq } from "drizzle-orm";
+import { Suspense } from "react";
+import {
+  DrawerProvider,
+  DrawerButton,
+  DrawerContent,
+} from "~/components/common/Drawer";
+import IconButton from "~/components/common/IconButton";
+import { Download, HomeSearch, Plus } from "~/components/common/Icons";
+import Loading from "~/components/common/Loading";
+import Search from "~/components/common/Search";
+import Table from "~/components/common/Table";
+import AddBuilding from "~/components/forms/AddBuilding";
+import DeleteBuilding from "~/components/forms/DeleteBuilding";
+import EditBuilding from "~/components/forms/EditBuilding";
+import useSearch from "~/hooks/useSearch";
+import { delay } from "~/utilties/client/delay";
+import { db } from "~/utilties/server/database/connection";
+import {
+  buildingTable,
+  roomTable,
+  residentTable,
+  zoneTable,
+  staffTable,
+  assistantStaffTable
+} from "~/utilties/server/database/schema";
+import { csv } from "~/utilties/client/csv";
+import { z } from "zod";
+import { build } from "vite";
+
+export async function loader() {
+  const data = await db
+    .select({
+      id: assistantStaffTable.id,
+      first: residentTable.firstName,
+      last: residentTable.lastName,
+      fullName: sql`concat(${residentTable.firstName}, ' ', ${residentTable.lastName})`.as("fullName"),
+      email: residentTable.emailAddress,
+      phone: residentTable.phoneNumber,
+      mailbox: residentTable.mailbox,
+      hometown: sql`concat(${residentTable.city}, ', ', ${residentTable.state})`.as("hometown"),
+      class: residentTable.class,
+      building: buildingTable.name,
+      roomBuilding: sql`concat(${buildingTable.name}, ' ', ${roomTable.roomNumber})`.as("roomBuilding"),
+      rd: sql`concat(${staffTable.firstName}, ' ', ${staffTable.lastName})`.as("rd")
+    })
+    .from(assistantStaffTable)
+    .leftJoin(residentTable, eq(assistantStaffTable.residentId, residentTable.id))
+    .leftJoin(staffTable, eq(assistantStaffTable.staffId, staffTable.id))
+    .leftJoin(roomTable, eq(residentTable.roomId, roomTable.id))
+    .leftJoin(buildingTable, eq(roomTable.buildingId, buildingTable.id));
+
+  return defer({
+    data: data,
+  });
+}
+
+export default function AdminPeopleARDsPage() {
+  const initialData = useLoaderData<typeof loader>();
+
+  return (
+    <Suspense fallback={<Loading />}>
+      <Await resolve={delay(initialData.data)}>
+        {(data) => {
+          const { handleSearch, filteredData } = useSearch(data);
+          return (
+            <section className="space-y-5">
+              <div className="flex">
+                <Search
+                  placeholder="Search for an ARD..."
+                  handleSearch={handleSearch}
+                />
+              </div>
+              <Table
+                columnKeys={{
+                  first: "First",
+                  last: "Last",
+                  building: "Building",
+                  rd: "RD"
+                }}
+                rows={filteredData || data}
+                rowKeys={{
+                  fullName: "Name",
+                  roomBuilding: "Room Number",
+                  email: "Email",
+                  phone: "Phone Number",
+                  mailbox: "Mailbox Number",
+                  hometown: "Hometown",
+                  class: "Class",
+                  rd: "RD",
+                }}
+                InstructionComponent={() => (
+                  <div className="w-2/5 p-5 space-y-3 flex flex-col items-center justify-center">
+                    <HomeSearch className="w-7 h-7" />
+                    <h2 className="text-xl font-bold">First Select an ARD</h2>
+                  </div>
+                )}
+                EditComponent={EditBuilding} //TODO
+                DeleteComponent={DeleteBuilding} //TODO
+              />
+            </section>
+          );
+        }}
+      </Await>
+    </Suspense>
+  );
+}
