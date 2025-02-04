adb3b65b0ace8da7d412529fdd724c4cb3f83488
hensleyas21 <hensleyas21@gcc.edu>
2025-02-04 11:31:34 -0500

    Rooms can be unassigned from an RA

--- app/schemas/housing/room.ts
+++ app/schemas/housing/room.ts
@@ -4,7 +4,7 @@ export const Room = z.object({
   id: z.coerce.number(),
   roomNumber: z.string(),
   buildingId: z.coerce.number(),
-  zoneId: z.coerce.number(),
+  zoneId: z.union([z.literal("null").transform(() => null), z.coerce.number()]),
   capacity: z.coerce.number(),
   roomType: z.enum(["UPPER_CAMPUS", "COLONIAL_QUAD", "COLONIAL_TRIPLE","COLONIAL_DOUBLE"])
 });
