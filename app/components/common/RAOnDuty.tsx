672f9c531219602e609b8c7f40bc5e72d16a9845
Liam Grossman <liam.j.grossman@gmail.com>
2025-02-04 21:31:14 -0500

    Add to on duty

--- app/components/common/RAOnDuty.tsx
+++ app/components/common/RAOnDuty.tsx
@@ -14,7 +14,7 @@ export default function RAOnDuty(props: RAOnDutyProps) {
     <div className="border rounded-lg divide-y">
       <div className="p-5 space-x-5 flex items-center">
         <img
-          src={`/avatars/ra_${raOnDuty.zoneId}.webp`}
+          src={`/avatars/ra/${raOnDuty.zoneId}`}
           alt={`${name}'s profile picture`}
           className="w-14 h-14 rounded-full"
         />
