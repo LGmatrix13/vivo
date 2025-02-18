61d9a95c59768f0697b52b23c017e6a42e8585c0
Kesterholtem21 <kesterholtem21@gcc.edu>
2025-02-17 20:15:53 -0500

    python embed file

---
+++ embedText.py
@@ -0,0 +1,13 @@
+import nltk
+import gensim.downloader as api 
+import numpy as np
+from nltk.tokenize import word_tokenize
+
+nltk.download('punkt')
+model = api.load("word2vec-google-news-300")
+def embed(text: str, model):
+    words = word_tokenize(text)
+    vectors = [model[word] for word in words if word in model]
+    if vectors:
+        return np.mean(vectors, axis=0).tolist()
+    return None
