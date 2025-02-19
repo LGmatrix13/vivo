d025b584c92e5e6e2ae7cb56fe277d801e371308
Kesterholtem21 <kesterholtem21@gcc.edu>
2025-02-19 13:20:22 -0500

    ollama fix for chunks

--- embedText.py
+++ embedText.py
@@ -1,21 +1,17 @@
 import json
-import shutil
-import nltk
-import gensim.downloader as api 
-import numpy as np
-from nltk.tokenize import word_tokenize
+import ollama
+from ollama import Client
 
-nltk.download('punkt')
-nltk.download('punkt_tab')
-model = api.load("word2vec-google-news-300")
+client = Client(
+  host='http://10.18.101.96:11434',
+)
 
-def embed(text: str, model):
-    words = word_tokenize(text)
-    vectors = [model[word] for word in words if word in model]
-    if vectors:
-        return np.mean(vectors, axis=0).tolist()
-    return None
- 
+
+def embed(text: str):
+    print('running chunk')
+    embedding=  client.embeddings(model='nomic-embed-text', prompt=text)
+    return embedding.embedding
+
 def split_text_into_chunks(file_path, num_chunks=200):
     with open(file_path, 'r', encoding='utf-8') as file:
         words = file.read().split()
@@ -39,7 +35,7 @@ def main():
     output_path = 'RAManual_output.json'  # Change this to your output file path
     
     chunks = split_text_into_chunks(file_path)
-    embedded_chunks = [{f"chunk_{i+1}": embed(chunk, model), "text": chunks[i]} for i, chunk in enumerate(chunks)]
+    embedded_chunks = [{f"chunk_{i+1}": embed(chunk), "text": chunks[i]} for i, chunk in enumerate(chunks)]
     
     with open(output_path, 'w', encoding='utf-8') as outfile:
         dump = json.dump(embedded_chunks, outfile, indent=4)
