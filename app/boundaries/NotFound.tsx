bbac72c15814fb66f19c88000218a6ea67c01d7c
Liam Grossman <liam.j.grossman@gmail.com>
2025-03-22 16:47:00 -0400

    fix button style

--- app/boundaries/NotFound.tsx
+++ app/boundaries/NotFound.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
       <h1 className="text-3xl font-bold">Page Not Found</h1>
       <p>The page you are looking for does not exist.</p>
       <Link to="/auth/login">
-        <button className="bg-blue-600 hover:bg-blue-700 transition ease-in-out h-10 px-3 rounded-full text-white">
+        <button className="bg-blue-600 hover:bg-blue-700 transition ease-in-out h-12 px-3 rounded-lg text-white">
           Go Home
         </button>
       </Link>
