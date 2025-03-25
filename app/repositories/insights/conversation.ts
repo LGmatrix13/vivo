dfec7c5d312e3ed84f410bbf262e3d34a6b674b2
Kesterholtem21 <kesterholtem21@gcc.edu>
2025-03-25 11:20:48 -0400

    small change to conversation insights repo

--- app/repositories/insights/conversation.ts
+++ app/repositories/insights/conversation.ts
@@ -235,6 +235,7 @@ export async function readConversationInsightsLastConversatonsAsRA(
       ({
         title: `${insight.daysSinceLastConvo} days since your last conversation with ${insight.name}`,
         level: calculateLevel(insight.daysSinceLastConvo),
+        href: `/ra/reports/conversation?name=${insight.name}`,
         action: {
           title: "Create a Conversation",
           //TODO add search parameter by the students name
