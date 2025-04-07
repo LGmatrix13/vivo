ff2d58e18dabe8660aae29a64cd3dbd27e5f58bf
hensleyas21 <hensleyas21@gcc.edu>
2025-04-07 13:25:41 -0400

    repositories/read comments

--- app/repositories/read/reports.ts
+++ app/repositories/read/reports.ts
@@ -1,10 +1,12 @@
-import { Conversation } from "~/schemas/reports/conversation";
 import { ReadReport } from "~/schemas/reports/readReport";
 import { db } from "~/utilties/postgres.server";
 import { readTable } from "~/utilties/schema.server";
 
 type Values = { [key: string]: any };
 
+/**
+ * Creates a new record in the database for a report
+ */
 export async function createReadReport(values: Values, request: Request) {
   return await db.insert(request, readTable, ReadReport, values, false);
 }
