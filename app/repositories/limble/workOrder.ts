565737b1317adb7781a0a1a70b3dd10556fbfa02
Kesterholtem21 <kesterholtem21@gcc.edu>
2025-04-07 20:23:46 -0400

    add comments to the limble repo

--- app/repositories/limble/workOrder.ts
+++ app/repositories/limble/workOrder.ts
@@ -1,14 +1,21 @@
 import { eq } from "drizzle-orm";
-import { number } from "zod";
 import { db } from "~/utilties/postgres.server";
 import { LimbleTable } from "~/utilties/schema.server";
 
+/**
+ * deletes a work order from the database
+ * @returns whether or not that deletion was a success or not
+ */
 export async function deleteWorkOrder(id: number) {
   return await db.client
     .delete(LimbleTable)
     .where(eq(LimbleTable.workOrderId, id));
 }
 
+/**
+ * creates a new work order in the database
+ * @returns whether or not that creation was a success or not
+ */
 export async function createWorkOrder(roomId: number, workOrderId: number) {
   return await db.client.insert(LimbleTable).values({
     roomId,
